673ea4ce753e96912a7fd789765976ed56283fbb
rahul011097 <rahul011097@gmail.com>
2025-09-08 16:20:09 +0530

    chat screen

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
+
+
+const users = {};
+
 //database connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
