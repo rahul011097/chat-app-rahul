77fc7b8aeba58ead715f4a8958a7c114ed4bc9c4
rahul011097 <rahul011097@gmail.com>
2025-09-16 16:29:31 +0530

    push

--- server/server.js
+++ server/server.js
@@ -5,12 +5,25 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const {Server} = require("socket.io");
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase/serviceAccountKey.json");
 
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
+
+
 
 
 const app = express();
